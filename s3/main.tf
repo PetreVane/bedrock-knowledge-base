dd7a4c3a197eaf92e1b178dab7bffff3a83d2ba1
Petre Vane <petre.vane@gmail.com>
2024-09-04 14:52:52 +0200

    Add S3 bucket and outputs for knowledge base
    - Create S3 bucket resource with random ID for unique naming.
    - Implement public access block for the S3 bucket to enhance security.
    - Define output for the bucket ARN in outputs.tf.

---
+++ s3/main.tf
@@ -0,0 +1,17 @@
+
+resource "random_id" "generate" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "knowledge_base_bucket" {
+  bucket = "tf-bedrock-knowledge-base-${random_id.generate.hex}"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_access" {
+  bucket = aws_s3_bucket.knowledge_base_bucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
---
+++ s3/outputs.tf
@@ -0,0 +1,4 @@
+
+output "kb_bucket_arn" {
+  value = aws_s3_bucket.knowledge_base_bucket.arn
+}
