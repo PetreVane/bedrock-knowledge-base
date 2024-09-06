0a0de58d7a7ac20d674da7d9c6630cd018022fd7
Petre Vane <petre.vane@gmail.com>
2024-09-06 10:03:14 +0200

    Format code for consistency and readability in main.tf file

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 }
 
 provider "pinecone" {
-  api_key     = var.pinecone_api_key
+  api_key = var.pinecone_api_key
 }
 
 module "s3" {
@@ -24,31 +24,32 @@ module "s3" {
 }
 
 module "secrets_manager" {
-  source = "./secrets_manager"
+  source           = "./secrets_manager"
   pinecone_api_key = var.pinecone_api_key
+  kb_name          = module.iam.bedrock_kb_role_name
 }
 
 module "iam" {
   source               = "./iam"
-  region = var.region
+  region               = var.region
   kb_source_bucket_arn = module.s3.kb_bucket_arn
-  pinecone_secret_arn = module.secrets_manager.pinecone_secret_arn
-  embedings_model_arn = module.bedrock.embedings_model_arn
-  knowledge_base_arn = module.bedrock.knowledge_base_arn
+  pinecone_secret_arn  = module.secrets_manager.pinecone_secret_arn
+  embedings_model_arn  = module.bedrock.embedings_model_arn
+  knowledge_base_arn   = module.bedrock.knowledge_base_arn
 }
 
 module "pinecone" {
-  source           = "./pinecone_vectorDB"
+  source               = "./pinecone_vectorDB"
   pinecone_environment = var.pinecone_environment
 }
 
 module "bedrock" {
   source = "./bedrock_knowledge_base"
-  
+
   knowledge_base_role_arn        = module.iam.bedrock_kb_role_arn
   pinecone_connection_string     = module.pinecone.pinecone_host
   pinecone_credential_secret_arn = module.secrets_manager.pinecone_secret_arn
   pinecone_index_name            = module.pinecone.pincone_index_name
   source_bucket_arn              = module.s3.kb_bucket_arn
-#   pinecone_api_key               = ""
+#   iam_policy_attachment_id       = module.iam.iam_policy_attachment_id
 }
