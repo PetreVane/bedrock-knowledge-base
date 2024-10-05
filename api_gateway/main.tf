5d5ec9ef6dc0a753404a1a7e290b0298faf69dae
Petre Vane <petre.vane@gmail.com>
2024-10-05 21:58:50 +0200

    Add API Gateway configuration and security measures:
    1. Adds a unique name to the API Gateway
    2. Implements a request validation
    3. Configures the API method with authorization and request models
    4. Sets up deployment and stage resources
    5. Creates an API key and usage plan
    6. Adds error handling for missing authentication tokens

--- api_gateway/main.tf
+++ api_gateway/main.tf
@@ -5,7 +5,7 @@ resource "random_id" "generator" {
 
 # Api Gateway
 resource "aws_api_gateway_rest_api" "kb_api" {
-  name = "knowledge_base_rest_api"
+  name = "knowledge_base_rest_api-${random_id.generator.hex}"
 }
 
 # Resources
@@ -15,16 +15,41 @@ resource "aws_api_gateway_resource" "kb_api_resource" {
   rest_api_id = aws_api_gateway_rest_api.kb_api.id
 }
 
-//TODO: add authorization to API Gateway
+# Request validator
+resource "aws_api_gateway_request_validator" "kb_request_validator" {
+  name        = "Request Body Validator"
+  rest_api_id = aws_api_gateway_rest_api.kb_api.id
+  validate_request_body       = true
+  validate_request_parameters = false
+}
+
+# Request validator model
+resource "aws_api_gateway_model" "kb_req_validator_model" {
+  content_type = "application/json"
+  name         = "2356"
+  description = "Validation model for KB API"
+  rest_api_id  = aws_api_gateway_rest_api.kb_api.id
+
+  schema = jsonencode({
+    type = "object"
+    required = ["query"]
+    properties = {
+      query = { type = "string" }
+    }
+  })
+}
+
 # Method
 resource "aws_api_gateway_method" "kb_api_method" {
-  authorization = "NONE" // change this
-  http_method   = "GET"
+  authorization = "NONE"
+  http_method   = "POST"
   resource_id   = aws_api_gateway_resource.kb_api_resource.id
   rest_api_id   = aws_api_gateway_rest_api.kb_api.id
 
-  request_parameters = {
-    "method.request.querystring.query" = true
+  api_key_required = true
+  request_validator_id = aws_api_gateway_request_validator.kb_request_validator.id
+  request_models = {
+    "application/json" = aws_api_gateway_model.kb_req_validator_model.name
   }
 }
 
@@ -42,7 +67,67 @@ resource "aws_api_gateway_integration" "kb_api_integration" {
 # Deployment
 resource "aws_api_gateway_deployment" "kb_api_deployment" {
   rest_api_id = aws_api_gateway_rest_api.kb_api.id
-  stage_name = var.deployment_env
 
-  depends_on = [aws_api_gateway_integration.kb_api_integration]
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.kb_api_resource.id,
+      aws_api_gateway_method.kb_api_method.id,
+      aws_api_gateway_integration.kb_api_integration.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
+
+# Stage
+resource "aws_api_gateway_stage" "kb_api_stage" {
+  deployment_id = aws_api_gateway_deployment.kb_api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.kb_api.id
+  stage_name    = var.deployment_env
+}
+
+# API Key
+resource "aws_api_gateway_api_key" "kb_api_key" {
+  name = "knowledge_base_api_key"
+  enabled = true
+}
+
+# Usage plan
+resource "aws_api_gateway_usage_plan" "kb_usage_plan" {
+  name = "knowledge_base_usage_plan"
+  api_stages {
+    api_id = aws_api_gateway_rest_api.kb_api.id
+    stage  = aws_api_gateway_stage.kb_api_stage.stage_name
+  }
+
+  quota_settings {
+    limit  = 1000
+    period = "MONTH"
+  }
+
+  throttle_settings {
+    burst_limit = 50
+    rate_limit = 50
+  }
+}
+
+# Associate the apy key with the usage plan
+resource "aws_api_gateway_usage_plan_key" "kb_usage_plan_association" {
+  key_id = aws_api_gateway_api_key.kb_api_key.id
+  key_type = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.kb_usage_plan.id
+}
+
+# Error message
+resource "aws_api_gateway_gateway_response" "kb_error_message" {
+  response_type = "MISSING_AUTHENTICATION_TOKEN"
+  rest_api_id   = aws_api_gateway_rest_api.kb_api.id
+  status_code = "403"
+
+  response_templates = {
+    "application/json" = "{\"message\": \"Missing or invalid API key\"}"
+  }
+}
+
